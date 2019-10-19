de4722dceaf686533336109ed0c3a409ea0526ea
a-yuto <arakiyutoam@gmail.com>
2019-10-19 10:27:44 +0000

    再利用できそうな関数をにlibrary.rsにまとめました

--- src/9.rs
+++ src/9.rs
@@ -1,11 +1,10 @@
-
 use rand::seq::SliceRandom;
 use std::iter::FromIterator;
 fn print_typename<T>(_: T) {
     println!("{}", std::any::type_name::<T>());
 }
 
-fn resolve_word(s: String) -> Vec<String> {
+fn resolve_word(s: &String) -> Vec<String> {
   let mut x: Vec<String> = Vec::new();
   let mut word: String = String::new();
   for ch in s.chars() {
@@ -19,22 +18,34 @@ fn resolve_word(s: String) -> Vec<String> {
   x
 }
 
-fn typoglycemia (original: &String ) -> String {
-  let sep_word: Vec<String> = resolve_word(original);
-  let unreadable = String::new();
-  for word in sep_word {
-    if word.chars().count > 4 {
-      let word_char: Vec<char>   = word.chars().collect();
-      let mut rng = rand::thread_rng();
-      word_char.shuffle(&mut rng);
-      word = String::from_iter(word_char);
-    }
-    format!("{}{}",unreadable,word);
-  }
-  unreadable
+fn prankstring(s: &String ) -> String {
+  let mut word_char: Vec<char>   = s.chars().collect();
+  let mut rng = rand::thread_rng();
+  word_char.shuffle(&mut rng);
+  let baka:String  = String::from_iter(word_char);
+  baka
 }
 
+fn typoglycemia (original: &String ) -> String {
+  let sep_word: Vec<String> = resolve_word(&original);
+  let mut unreadable = String::new();
+  for mut word in sep_word {
+    let size = word.chars().count();
+    if size > 4 {
+      let head = word.chars().nth(0).unwrap();
+      let last = word.chars().nth(size - 1).unwrap();
+      word.remove(size - 1);
+      word.remove(0);
+      word = prankstring(&word);
+      word = head.to_string() + &word + &last.to_string();
+    }
+    word.push_str(&" ".to_string());
+    unreadable.push_str(&word);
+  }         
+  unreadable
+}         
+        
 fn main(){
   let s: String = String::from("I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind .");
-  println!("{}",typoglucemia(&s));
+  println!("{}",typoglycemia(&s));
 }
